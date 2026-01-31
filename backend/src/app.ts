a4130d4297ec8372a0890a8b7419c182727fb270
Nitish Kumar <nitishpandit95087546@gmail.com>
2026-01-31 12:58:53 +0530

    integrate auth with clerk

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,28 @@
 import express from "express";
+import { clerkMiddleware } from "@clerk/express";
 
 import authRoutes from "./routes/authRoutes";
 import chatRoutes from "./routes/chatRoutes";
 import messageRoutes from "./routes/messageRoutes";
 import userRoutes from "./routes/userRoutes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(clerkMiddleware()); // Middleware that integrates Clerk authentication into your Express application. It checks the request's cookies and headers for a session JWT and, if found, attaches the Auth object to the request object under the auth key.
+
 app.get("/health", (req, res) => {
   res.json({ status: "ok", message: "Server is running up" });
 });
 
-app.use("/api/v1", authRoutes);
-app.use("/api/v1", chatRoutes);
-app.use("/api/v1", messageRoutes);
-app.use("/api/v1", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/user", userRoutes);
+
+// Error handlers must come after all routes and other middleware can catch the error passed with next(err)
+app.use(errorHandler);
 
 export default app;
